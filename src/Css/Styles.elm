f2f2b4aac8857d14abe0579f47ccc659c63a6ce3
Steven Vandevelde <icid.asset@gmail.com>
2017-02-04 11:32:21 +0100

    Restyle auth button

--- src/Css/Styles.elm
+++ src/Css/Styles.elm
@@ -53,15 +53,22 @@ stylesLocal =
         , lineHeight (Css.num 1.75)
         , textRendering optimizeLegibility
         ]
-      --
+      ------------------------------------------------------
+      -- Authentication button
+      ------------------------------------------------------
     , class AuthenticationButton
         [ alignItems center
-        , backgroundColor (hex "#fff")
+        , backgroundColor (rgba 0 0 0 0.45)
         , borderRadius (px 3)
+        , boxShadow4 (px 0) (px 0) (px 20) (rgba 255 255 255 0.05)
+        , color (hex "#fff")
         , cursor pointer
         , displayFlex
+        , fontSize (Css.rem 0.95)
+        , fontWeight (Css.int 300)
         , lineHeight (gr 1.5)
         , padding2 (gr 1) (gr 1.5)
+        , property "padding-top" "calc(.75rem + 1px)"
           --
         , descendants
             [ class AuthenticationButtonLogo
