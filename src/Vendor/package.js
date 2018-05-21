fd61a65e73fb982555cd3aadca013ccb234b4516
Steven Vandevelde <icid.asset@gmail.com>
2018-05-21 18:57:01 +0200

    Update JS dependencies

--- src/Vendor/package.js
+++ src/Vendor/package.js
@@ -6,7 +6,7 @@ self.blockstack = require("blockstack");
 self.camelcase = require("camelcase");
 self.elasticlunr = require("elasticlunr");
 self.encoding = require("text-encoding");
-self.RemoteStorage = require("remotestoragejs");
+self.RemoteStorage = require("remotestoragejs/release/remotestorage");
 
 if (self.document) {
   self.tocca = require("tocca");
