0896bd88cc88ef60fbfe5826acd93833b21eb1a5
Steven Vandevelde <icid.asset@gmail.com>
2020-06-14 00:11:37 +0200

    Improve album-cover lookup

--- src/Javascript/Brain/artwork.js
+++ src/Javascript/Brain/artwork.js
@@ -16,6 +16,7 @@ export function find(prep) {
   return findUsingTags(prep)
     .then(a => a ? a : findUsingMusicBrainz(prep))
     .then(a => a ? a : findUsingLastFm(prep))
+    .then(a => a ? a : findUsingMusicBrainz(prep, { albumOnly: true }))
     .then(a => a ? a : REJECT())
     .then(a => a.type.startsWith("image/") ? a : REJECT())
 }
@@ -45,26 +46,37 @@ function findUsingTags(prep) {
 // 2. MUSIC BRAINZ
 
 
-function findUsingMusicBrainz(prep) {
+function findUsingMusicBrainz(prep, options) {
+  options = options || {}
+
   const parts = atob(prep.cacheKey).split(" --- ")
   const artist = parts[0]
   const album = parts[1]
 
-  const query = `release:"${album}" AND artist:"${artist}"`
+  const query = `release:"${album}"` + (options.albumOnly ? `` : ` AND artist:"${artist}"`)
   const encodedQuery = encodeURIComponent(query)
 
   return fetch(`https://musicbrainz.org/ws/2/release/?query=${encodedQuery}&fmt=json`)
     .then(r => r.json())
-    .then(r => {
-      const release = r.releases[0]
-      if (!release) return null
+    .then(r => musicBrainzCover(r.releases))
+}
 
-      return fetch(
-        `https://coverartarchive.org/release/${release.id}/front-500`
-      ).then(
-        r => r.blob()
-      )
-    })
+
+function musicBrainzCover(remainingReleases) {
+  const release = remainingReleases[0]
+  if (!release) return null
+
+  return fetch(
+    `https://coverartarchive.org/release/${release.id}/front-500`
+  ).then(
+    r => r.blob()
+  ).then(
+    r => r && r.type.startsWith("image/")
+      ? r
+      : musicBrainzCover(remainingReleases.slice(1))
+  ).catch(
+    e => musicBrainzCover(remainingReleases.slice(1))
+  )
 }
 
 
@@ -77,12 +89,17 @@ function findUsingLastFm(prep) {
 
   return fetch(`https://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&api_key=4f0fe85b67baef8bb7d008a8754a95e5&format=json`)
     .then(r => r.json())
-    .then(r => {
-      const album = r.results.albummatches.album[0]
-      const url = album ? album.image[album.image.length - 1]["#text"] : null
+    .then(r => lastFmCover(r.results.albummatches.album))
+}
 
-      return url && url !== ""
-        ? fetch(url).then(r => r.blob())
-        : null
-    })
+
+function lastFmCover(remainingMatches) {
+  const album = remainingMatches[0]
+  const url = album ? album.image[album.image.length - 1]["#text"] : null
+
+  return url && url !== ""
+    ? fetch(url)
+        .then(r => r.blob())
+        .catch(_ => lastFmCover(remainingMatches.slice(1)))
+    : album && lastFmCover(remainingMatches.slice(1))
 }
