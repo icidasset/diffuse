ccc0c55090e9d7c66257f3467c5a53c84a71268e
Steven Vandevelde <icid.asset@gmail.com>
2020-07-04 19:19:10 +0200

    Closes #220

--- src/Applications/UI/Sources/State.elm
+++ src/Applications/UI/Sources/State.elm
@@ -479,6 +479,7 @@ toggleActivation { sourceId } model =
             )
         |> (\collection -> { model | sources = collection })
         |> Tracks.reviseCollection Tracks.Collection.identify
+        |> andThen Common.forceTracksRerender
         |> andThen User.saveSources
 
 
