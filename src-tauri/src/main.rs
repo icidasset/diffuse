6039d78efbd05a9efd4bd25d3c4b968f694c7bd5
Steven Vandevelde <icid.asset@gmail.com>
2022-01-29 18:23:44 +0100

    Add menu items to tauri app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 )]
 
 use tauri::{App, PhysicalPosition, PhysicalSize, Position, Size};
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 use tauri::{Window, WindowBuilder, WindowUrl};
 
 fn main() {
@@ -14,6 +15,7 @@ fn main() {
         .setup(move |app| {
             let w = create_window(app, port).unwrap();
 
+            // Scale window to a bit smaller than screen size
             let monitor = w.current_monitor().unwrap().unwrap();
             let screen_size = tauri::window::Monitor::size(&monitor);
 
@@ -23,29 +25,67 @@ fn main() {
             }))
             .unwrap();
 
-            w.set_position(Position::Physical(PhysicalPosition { x: 30, y: 30 }))
-                .unwrap();
+            // Put the window in the middle of the screen
+            let window_offset = Position::Physical(PhysicalPosition { x: 30, y: 30 });
 
+            w.set_position(window_offset).unwrap();
+
+            // Fin
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+/**
+ * Create a window that loads our localhost server.
+ */
 fn create_window(app: &mut App, port: u16) -> tauri::Result<Window> {
     return app.create_window(
         "Diffuse",
         WindowUrl::External(format!("http://localhost:{}", port).parse().unwrap()),
         |window_builder, webview_attributes| {
             let w = window_builder
-                .title("Diffuse")
                 .maximized(true)
-                // .position(10.0, 10.0)
-                // .inner_size(screen_size.width - 20, screen_size.height - 20)
+                .menu(menu())
                 .resizable(true)
+                .title("Diffuse")
                 .transparent(true);
 
             (w, webview_attributes)
         },
     );
+}
+
+/**
+ * Menu
+ */
+fn menu() -> Menu {
+    let app_menu = Menu::new()
+        .add_native_item(MenuItem::Hide)
+        .add_native_item(MenuItem::Services)
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::Quit);
+
+    let file_menu = Menu::new().add_native_item(MenuItem::CloseWindow);
+
+    let edit_menu = Menu::new()
+        .add_native_item(MenuItem::Undo)
+        .add_native_item(MenuItem::Redo)
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::Cut)
+        .add_native_item(MenuItem::Copy)
+        .add_native_item(MenuItem::Paste)
+        .add_native_item(MenuItem::SelectAll);
+
+    let window_menu = Menu::new()
+        .add_native_item(MenuItem::Minimize)
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::ShowAll);
+
+    return Menu::new()
+        .add_submenu(Submenu::new("Diffuse", app_menu))
+        .add_submenu(Submenu::new("File", file_menu))
+        .add_submenu(Submenu::new("Edit", edit_menu))
+        .add_submenu(Submenu::new("Window", window_menu));
 }
