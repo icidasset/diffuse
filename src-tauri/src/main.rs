fa3f0e623c27aeb3c0fe87148d6d1fe8c49758c1
Steven Vandevelde <icid.asset@gmail.com>
2022-08-06 20:09:59 +0200

    Hack around the no globalTauri issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,24 +4,36 @@
 )]
 
 
-use tauri::{WindowBuilder, WindowUrl};
+use tauri::{utils::config::AppUrl, WindowBuilder, WindowUrl};
+
+
+// #[tauri::command]
+// fn my_command(args: u64) -> Result<String, ()> {
+//   println!("executed command with args {:?}", args);
+//   Ok("executed".into())
+// }
 
 
 fn main() {
     let port = 44999;
-    let context = tauri::generate_context!("tauri.conf.json");
+    // let http = tauri_invoke_http::Invoke::new(["http://localhost:44998"]);
+    let mut context = tauri::generate_context!("tauri.conf.json");
+
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+    context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
 
     tauri::Builder::default()
+        // .invoke_system(http.initialization_script(), http.responder())
+        // .invoke_handler(tauri::generate_handler![my_command])
         .plugin(tauri_plugin_localhost::Builder::new(port).build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .setup(move |app| {
-            WindowBuilder::new(
-                app,
-                "main",
-                WindowUrl::External(
-                    format!("http://localhost:{}", port).parse().unwrap()
-                )
-            )
+            // http.start(app.handle());
+
+            WindowBuilder::new(app, "main", window_url)
                 .title("Diffuse")
                 .maximized(true)
                 .resizable(true)
@@ -31,18 +43,6 @@ fn main() {
             // Fin
             Ok(())
         })
-        // .on_window_event(|event| match event.event() {
-
-        //     tauri::WindowEvent::ThemeChanged(theme) => {
-        //         if !focused {
-        //             event.window().hide().unwrap();
-        //         }
-
-        //     }
-
-        //     _ => {}
-
-        // })
         .run(context)
         .expect("Error while running tauri application");
 }
