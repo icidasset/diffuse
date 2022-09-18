1a948e1ef561c453eaa43a184b060deeb1b03282
Steven Vandevelde <icid.asset@gmail.com>
2022-09-18 16:18:34 +0200

    Fix yet another M1 issue with stack

--- nix/shell.nix
+++ nix/shell.nix
@@ -15,9 +15,30 @@
   # -----
   # Inspired by https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
 
+  # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
+  m1HaskellPackages =
+    pkgs.haskellPackages.override {
+      overrides = self: super:
+        let
+          workaround140774 = hpkg: with pkgs.haskell.lib;
+            overrideCabal hpkg (drv: {
+              enableSeparateBinOutput = false;
+            });
+        in
+        {
+          ghcid = workaround140774 super.ghcid;
+          stack = workaround140774 super.stack;
+        };
+    };
+
   stack-wrapped = symlinkJoin {
     name = "stack";
-    paths = [ haskellPackages.stack ];
+    paths =
+      [ (if isM1Mac
+          then m1HaskellPackages.stack
+          else haskellPackages.stack
+        )
+      ];
     buildInputs = [ makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/stack \
