830c35ea2abbc391accf96d145ce1a1a319ffe39
Steven Vandevelde <icid.asset@gmail.com>
2022-09-24 17:27:13 +0200

    Nix config tweaks

--- nix/shell.nix
+++ nix/shell.nix
@@ -11,45 +11,6 @@
     rustc = rust;
   };
 
-  # Wraps
-  # -----
-  # Inspired by https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
-
-  # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
-  m1HaskellPackages =
-    pkgs.haskellPackages.override {
-      overrides = self: super:
-        let
-          workaround140774 = hpkg: with pkgs.haskell.lib;
-            overrideCabal hpkg (drv: {
-              enableSeparateBinOutput = false;
-            });
-        in
-        {
-          ghcid = workaround140774 super.ghcid;
-          stack = workaround140774 super.stack;
-        };
-    };
-
-  stack-wrapped = symlinkJoin {
-    name = "stack";
-    paths =
-      [ (if isM1Mac
-          then m1HaskellPackages.stack
-          else haskellPackages.stack
-        )
-      ];
-    buildInputs = [ makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/stack \
-        --add-flags "\
-          --nix \
-          --no-nix-pure \
-          --nix-shell-file=nix/stack-integration.nix \
-        "
-    '';
-  };
-
   # Dependencies
   # ------------
 
@@ -68,7 +29,7 @@
       nodejs-18_x
       nodePackages.pnpm
       rust
-      stack-wrapped
+      stack
     ];
 
     tauri = {
