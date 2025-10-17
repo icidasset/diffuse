97f7816d01f18311f97c9e0d490fef11f24f67b6
Steven Vandevelde <steven@tokono.ma>
2025-10-17 18:27:08 +0200

    feat: shared worker

--- src/common/worker.js
+++ src/common/worker.js
@@ -4,37 +4,79 @@ import { getTransferables } from "@okikio/transferables";
 import { xxh32 } from "xxh32";
 
 /**
- * @import {NodeWorkerOrNodeMessagePort} from "@mys/m-rpc";
+ * @import {NodeWorkerOrNodeMessagePort, WorkerGlobalScope} from "@mys/m-rpc";
  * @import {Announcement} from "./worker.d.ts"
  */
 
-export const define = defineWorkerFn;
-export const use = useWorkerFn;
+////////////////////////////////////////////
+// MISC
+////////////////////////////////////////////
+
+/**
+ * Manage incoming connections for a shared worker.
+ * If a regular worker is used instead, it'll just execute the callback immediately.
+ *
+ * @template {MessagePort | Worker | WorkerGlobalScope} T
+ * @param {(context: MessagePort | T) => void} callback
+ * @param {T} [context] Uses `globalThis` by default.
+ */
+export function ostiary(
+  callback,
+  context = /** @type {T} */ (/** @type {unknown} */ (globalThis)),
+) {
+  if (/** @type {any} */ (context).onmessage === null) {
+    return callback(context);
+  }
+
+  context.addEventListener(
+    "connect",
+    /**
+     * @param {any} event
+     */
+    (event) => {
+      /** @type {MessagePort} */
+      const port = event.ports[0];
+      port.start();
+
+      // Initiate setup
+      callback(port);
+    },
+  );
+}
+
+////////////////////////////////////////////
+// RAW
+////////////////////////////////////////////
 
 /**
  * @template T
  * @param {string} name
  * @param {T} args
+ * @param {MessagePort | Worker | WorkerGlobalScope} [context] Uses `globalThis` by default.
  */
-export function announce(name, args) {
+export function announce(
+  name,
+  args,
+  context = /** @type {WorkerGlobalScope} */ (globalThis),
+) {
   const transferables = getTransferables(args);
-  globalThis.postMessage(constructMsg(name, args), { transfer: transferables });
+  context.postMessage(constructMsg(name, args), { transfer: transferables });
 }
 
 /**
  * @template T
  * @param {string} name
  * @param {(args: T) => void} fn
- * @param {Worker | NodeWorkerOrNodeMessagePort} context
+ * @param {MessagePort | Worker | WorkerGlobalScope} [context]
  */
 export function listen(
   name,
   fn,
-  context = globalThis,
+  context = /** @type {WorkerGlobalScope} */ (globalThis),
 ) {
   context.addEventListener(
     "message",
-    /** @param {MessageEvent} event */ (event) => {
+    /** @param {any} event */ (event) => {
       const announcement = /** @type {Announcement<T>} */ (event.data);
       const { ns, type } = announcement;
 
@@ -47,7 +89,38 @@ export function listen(
 }
 
 ////////////////////////////////////////////
-// PRIVATE
+// RPC
+////////////////////////////////////////////
+
+/**
+ * @template {(...args: any[]) => any} Fn
+ * @param {string} name
+ * @param {Fn} fn
+ * @param {MessagePort | Worker | WorkerGlobalScope} [context] Uses `globalThis` by default.
+ */
+export function define(
+  name,
+  fn,
+  context = /** @type {WorkerGlobalScope} */ (globalThis),
+) {
+  return defineWorkerFn(name, fn, {
+    port: /** @type {any} */ (context),
+  });
+}
+
+/**
+ * @param {string} name
+ * @param {MessagePort | Worker | WorkerGlobalScope} [context] Uses `globalThis` by default.
+ */
+export function use(
+  name,
+  context = /** @type {WorkerGlobalScope} */ (globalThis),
+) {
+  return useWorkerFn(name, /** @type {any} */ (context));
+}
+
+////////////////////////////////////////////
+// ⛔️
 ////////////////////////////////////////////
 
 const ANNOUNCEMENT = "announcement";
