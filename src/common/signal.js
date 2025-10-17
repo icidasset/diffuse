0f62dff9e600852574547cae5f9062b06cb3d9f4
Steven Vandevelde <steven@tokono.ma>
2025-10-17 16:08:19 +0200

    feat: deep diff signals

--- src/common/signal.js
+++ src/common/signal.js
@@ -1,1 +1,27 @@
+import deepDiff from "@fry69/deep-diff";
+import { signal as alienSignal } from "alien-signals";
+
 export * from "alien-signals";
+
+/**
+ * @import {Signal} from "./signal.d.ts"
+ */
+
+/**
+ * @template T
+ * @param {T} initialValue
+ * @returns {Signal<T>}
+ */
+export function signal(initialValue) {
+  const s = alienSignal(initialValue);
+  const isPrimitive = Object(initialValue) !== initialValue;
+  if (isPrimitive) return s;
+
+  return /** @type {Signal<T>} */ ((b) => {
+    const a = s();
+    if (b === undefined) return a;
+
+    const diff = deepDiff(a, b);
+    if (diff) s(b);
+  });
+}
