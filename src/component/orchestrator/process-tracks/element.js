5d82eca439c32f1ed4bf587ca26379f9f0e87077
Steven Vandevelde <steven@tokono.ma>
2025-10-24 22:28:13 +0200

    refactor: orchestrators

--- src/component/orchestrator/process-tracks/element.js
+++ src/component/orchestrator/process-tracks/element.js
@@ -1,6 +1,6 @@
 import deepDiff from "@fry69/deep-diff";
 
-import { DiffuseElement } from "@common/element.js";
+import { DiffuseElement, query } from "@common/element.js";
 import { signal } from "@common/signal.js";
 
 /**
@@ -15,25 +15,14 @@ class ProcessTracksOrchestrator extends DiffuseElement {
   constructor() {
     super();
 
-    this.inputSelector = this.getAttribute("input-selector");
-    this.outputSelector = this.getAttribute("output-selector");
-    this.metadataProcessorSelector = this.getAttribute(
-      "metadata-processor-selector",
-    );
+    /** @type {InputElement} */
+    this.input = query(this, "input-selector");
 
-    if (!this.inputSelector) {
-      throw new Error("Missing required `input-selector` attribute");
-    }
+    /** @type {OutputElement} */
+    this.output = query(this, "output-selector");
 
-    if (!this.outputSelector) {
-      throw new Error("Missing required `output-selector` attribute");
-    }
-
-    if (!this.metadataProcessorSelector) {
-      throw new Error(
-        "Missing required `metadata-processor-selector` attribute",
-      );
-    }
+    /** @type {import("@component/processor/metadata/element.js").CLASS} */
+    this.metadataProcessor = query(this, "metadata-processor-selector");
   }
 
   // SIGNALS
@@ -52,61 +41,37 @@ class ProcessTracksOrchestrator extends DiffuseElement {
   async connectedCallback() {
     super.connectedCallback();
 
-    /** @type {OutputElement | null} */
-    const output = document.querySelector(this.outputSelector);
-
-    // Check output element presence
-    if (!output) throw new Error("Missing required `output` element");
-
     // Wait until defined
-    await customElements.whenDefined(output.localName);
+    await customElements.whenDefined(this.output.localName);
 
-    // Process whenever tracks are loaded
+    // Process whenever tracks are initially loaded
     this.effect(() => {
-      const state = output.tracks.state();
+      const state = this.output.tracks.state();
       if (state !== "loaded") return;
 
-      this.process(output);
+      this.process();
     });
   }
 
-  /**
-   * @param {OutputElement} output
-   */
-  async process(output) {
-    /** @type {InputElement | null} */
-    const input = document.querySelector(this.inputSelector);
+  // ACTIONS
 
-    // TODO
-    /** @type {any} */
-    const metadataProcessor = document.querySelector(
-      this.metadataProcessorSelector,
-    );
-
-    // Check element presence
-    if (!input) throw new Error("Missing required `input` element");
-    if (!metadataProcessor) {
-      throw new Error("Missing required `metadata-processor` element");
-    }
-
-    // Wait until defined
-    await customElements.whenDefined(input.localName);
-    await customElements.whenDefined(metadataProcessor.localName);
+  async process() {
+    await customElements.whenDefined(this.input.localName);
+    await customElements.whenDefined(this.metadataProcessor.localName);
 
     // Start
     this.#isProcessing.value = true;
     console.log("🪵 Processing initiated");
 
-    const cachedTracks = output.tracks.collection();
+    const cachedTracks = this.output.tracks.collection();
 
     // Contextualize
-    await input.contextualize(cachedTracks);
+    await this.input.contextualize(cachedTracks);
 
     // List
-    const tracks = await input.list(cachedTracks);
+    const tracks = await this.input.list(cachedTracks);
 
     // Fetch metadata if needed
-    // TODO: Parallelisation
     const tracksWithMetadata = await tracks.reduce(
       /**
        * @param {Promise<Track[]>} promise
@@ -117,12 +82,18 @@ class ProcessTracksOrchestrator extends DiffuseElement {
 
         if (track.tags && track.stats) return [...acc, track];
 
-        const resGet = await input.resolve({ method: "GET", uri: track.uri });
-        const resHead = await input.resolve({ method: "HEAD", uri: track.uri });
+        const resGet = await this.input.resolve({
+          method: "GET",
+          uri: track.uri,
+        });
+        const resHead = await this.input.resolve({
+          method: "HEAD",
+          uri: track.uri,
+        });
 
         if (!resGet) return [...acc, track];
 
-        const { stats, tags } = await metadataProcessor.supply({
+        const { stats, tags } = await this.metadataProcessor.supply({
           urls: { get: resGet.url, head: resHead?.url || resGet.url },
         });
 
@@ -136,7 +107,7 @@ class ProcessTracksOrchestrator extends DiffuseElement {
     const changed = !!diff;
 
     // Save if changed
-    if (changed) await output.tracks.save(tracksWithMetadata);
+    if (changed) await this.output.tracks.save(tracksWithMetadata);
 
     // Fin
     console.log("🪵 Processing completed");
