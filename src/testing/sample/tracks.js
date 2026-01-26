a97645ce0f239cbd174852c5103b576787319722
Steven Vandevelde <steven@tokono.ma>
2026-01-26 17:07:52 +0100

    test: search processor

--- src/testing/sample/tracks.js
+++ src/testing/sample/tracks.js
@@ -5,19 +5,27 @@
 /**
  * @type {Track}
  */
-const trackA = {
+export const trackA = {
   $type: "sh.diffuse.output.tracks",
   id: "sample-a",
   uri: "http://example.com/audio-a.mp3",
+  tags: {
+    artist: "Artist",
+    title: "Sample",
+  },
 };
 
 /**
  * @type {Track}
  */
-const trackB = {
+export const trackB = {
   $type: "sh.diffuse.output.tracks",
   id: "sample-b",
   uri: "http://example.com/audio-b.mp3",
+  tags: {
+    album: "B-side",
+    title: "Unknown",
+  },
 };
 
 export const tracks = [
---
+++ tests/components/processor/search/test.ts
@@ -0,0 +1,67 @@
+import { describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+
+import { testWeb } from "@tests/common/index.ts";
+import { trackA, trackB } from "@src/testing/sample/tracks.js";
+
+describe("components/processor/search", () => {
+  it("finds tracks by album", async () => {
+    const results = await testWeb(async () => {
+      const SearchProcessor = await import(
+        "@components/processor/search/element.js"
+      );
+      const processor = new SearchProcessor.CLASS();
+
+      document.body.append(processor);
+
+      // Add sample tracks to the supply first
+      const { tracks } = await import("@src/testing/sample/tracks.js");
+      await processor.supply({ tracks });
+
+      // Search for a specific term
+      return processor.search({ term: tracks[1]?.tags?.album });
+    });
+
+    expect(results[0]?.id).toBe(trackB.id);
+  });
+
+  it("finds tracks by artist", async () => {
+    const results = await testWeb(async () => {
+      const SearchProcessor = await import(
+        "@components/processor/search/element.js"
+      );
+      const processor = new SearchProcessor.CLASS();
+
+      document.body.append(processor);
+
+      // Add sample tracks to the supply first
+      const { tracks } = await import("@src/testing/sample/tracks.js");
+      await processor.supply({ tracks });
+
+      // Search for a specific term
+      return processor.search({ term: tracks[0]?.tags?.artist });
+    });
+
+    expect(results[0]?.id).toBe(trackA.id);
+  });
+
+  it("finds tracks by title", async () => {
+    const results = await testWeb(async () => {
+      const SearchProcessor = await import(
+        "@components/processor/search/element.js"
+      );
+      const processor = new SearchProcessor.CLASS();
+
+      document.body.append(processor);
+
+      // Add sample tracks to the supply first
+      const { tracks } = await import("@src/testing/sample/tracks.js");
+      await processor.supply({ tracks });
+
+      // Search for a specific term
+      return processor.search({ term: tracks[1]?.tags?.title });
+    });
+
+    expect(results[0]?.id).toBe(trackB.id);
+  });
+});
