966299497a9315f8e024dd6da30ebb9dd86159e9
Steven Vandevelde <steven@tokono.ma>
2026-02-05 23:15:01 +0100

    feat: playlist definition

--- src/definitions/types.d.ts
+++ src/definitions/types.d.ts
@@ -2,11 +2,15 @@ export type {
   Main as Constituent,
 } from "./types/sh/diffuse/output/constituent.ts";
 
+export type { Main as Favourite } from "./types/sh/diffuse/output/favourite.ts";
+
 export type {
+  AutoGenerate as PlaylistAutoGeneration,
   Criterion,
-  Main as Favourite,
+  Item as PlaylistItem,
+  Main as Playlist,
   Transformation,
-} from "./types/sh/diffuse/output/favourite.ts";
+} from "./types/sh/diffuse/output/playlist.ts";
 
 export type {
   Main as Track,
