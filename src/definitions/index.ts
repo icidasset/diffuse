e629e4a45ca57a989c9610eee95672b4639b94c9
Steven Vandevelde <steven@tokono.ma>
2026-02-07 22:49:39 +0100

    feat: add url to constituent definition + theme definition

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -1,3 +1,4 @@
 export * as ShDiffuseOutputConstituent from "./types/sh/diffuse/output/constituent.js";
 export * as ShDiffuseOutputPlaylist from "./types/sh/diffuse/output/playlist.js";
+export * as ShDiffuseOutputTheme from "./types/sh/diffuse/output/theme.js";
 export * as ShDiffuseOutputTrack from "./types/sh/diffuse/output/track.js";
