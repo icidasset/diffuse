9947a44c7638440763bb79d72bd12dd48d14b59c
Steven Vandevelde <steven@tokono.ma>
2026-02-13 18:11:41 +0100

    feat: webamp output element tabs

--- src/themes/webamp/configurators/output/element.js
+++ src/themes/webamp/configurators/output/element.js
@@ -19,13 +19,92 @@ class OutputConfig extends DiffuseElement {
     return html`
       <link rel="stylesheet" href="styles/vendor/98.css" />
 
-      <form>
-        <p>Where do you want to keep your data?</p>
-        <div class="field-row">
-          <input id="idb-json" type="radio" checked />
-          <label for="idb-json">Local only</label>
+      <style>
+      @import "./themes/webamp/98-vars.css";
+
+      #tabbed {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+      }
+
+      .window {
+        flex: 1;
+        overflow-y: auto;
+      }
+
+      /* TABS */
+
+      menu[role="tablist"] {
+        padding-top: 2px;
+
+        li > label {
+          cursor: pointer;
+          display: block;
+          padding: var(--radio-label-spacing);
+        }
+
+        /* Copied styles from "li[aria-selected=true]" */
+        li:has(input:checked) {
+          padding-bottom: 2px;
+          margin-top: -2px;
+          background-color: var(--surface);
+          position: relative;
+          z-index: 8;
+          margin-left: -3px;
+        }
+
+        input {
+          display: none
+        }
+      }
+
+      .window-body {
+        display: none
+      }
+
+      #tabbed:has(#overview-tab:checked) #overview-contents { display: block }
+      #tabbed:has(#automerge-tab:checked) #automerge-contents { display: block }
+      </style>
+
+      <div id="tabbed">
+        <menu role="tablist" class="multirows">
+          <li role="tab">
+            <label for="overview-tab">
+              <span>Overview</span>
+              <input name="output-tab" id="overview-tab" type="radio" checked="" />
+            </label>
+          </li>
+          <li role="tab">
+            <label for="automerge-tab">
+              <span>Automerge Repo</span>
+              <input name="output-tab" id="automerge-tab" type="radio" />
+            </label>
+          </li>
+        </menu>
+
+        <div class="window" role="tabpanel">
+          <!-- Overview -->
+          <div class="window-body" id="overview-contents">
+            <form>
+              <p>Do you want to sync your data somewhere?</p>
+              <div class="field-row">
+                <input id="idb-json" type="radio" checked />
+                <label for="idb-json">No syncing, just keep data locally.</label>
+              </div>
+              <div class="field-row">
+                <input id="idb-json" type="radio" disabled />
+                <label for="idb-json">Automerge Repo</label>
+              </div>
+            </form>
+          </div>
+
+          <!-- Automerge -->
+          <div class="window-body" id="automerge-contents">
+            <p>TODO</p>
+          </div>
         </div>
-      </form>
+      </div>
     `;
   }
 }
