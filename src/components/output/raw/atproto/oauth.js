f7d7ffe53e0f538cd1d334eb0c852846d8933b55
Steven Vandevelde <steven@tokono.ma>
2026-02-18 22:38:34 +0100

    fix: add missing ending slash in oauth redirect path

--- src/components/output/raw/atproto/oauth.js
+++ src/components/output/raw/atproto/oauth.js
@@ -47,7 +47,7 @@ if (!isLocalDev) {
   const url = new URL(location.href);
   const params = url.searchParams.entries();
 
-  redirect_uri = location.origin + "/oauth/redirect?" + QS.stringify({
+  redirect_uri = location.origin + "/oauth/redirect/?" + QS.stringify({
     ...params,
     redirect_path: location.pathname,
     variant: url.pathname.replace(/(^\/+|\/+$)/g, "").split("/")[0],
