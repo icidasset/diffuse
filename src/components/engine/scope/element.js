f40e850ec76682f23c25f6a43b5ad01c98c21b83
Steven Vandevelde <steven@tokono.ma>
2026-03-05 18:13:38 +0100

    feat: more scopes

--- src/components/engine/scope/element.js
+++ src/components/engine/scope/element.js
@@ -10,13 +10,17 @@ class ScopeEngine extends BroadcastableDiffuseElement {
 
   // SIGNALS
 
+  #groupBy = signal(/** @type {string | undefined} */ (undefined));
   #playlist = signal(/** @type {string | undefined} */ (undefined));
   #searchTerm = signal(/** @type {string | undefined} */ (undefined));
   #sortBy = signal(/** @type {string[]} */ ([]));
+  #sortDirection = signal(/** @type {string | undefined} */ (undefined));
 
+  groupBy = this.#groupBy.get;
   playlist = this.#playlist.get;
   searchTerm = this.#searchTerm.get;
   sortBy = this.#sortBy.get;
+  sortDirection = this.#sortDirection.get;
 
   // LIFECYCLE
 
@@ -27,15 +31,19 @@ class ScopeEngine extends BroadcastableDiffuseElement {
     // Broadcast if needed
     if (this.hasAttribute("group")) {
       const actions = this.broadcast(this.identifier, {
+        setGroupBy: { strategy: "replicate", fn: this.setGroupBy },
         setPlaylist: { strategy: "replicate", fn: this.setPlaylist },
         setSearchTerm: { strategy: "replicate", fn: this.setSearchTerm },
         setSortBy: { strategy: "replicate", fn: this.setSortBy },
+        setSortDirection: { strategy: "replicate", fn: this.setSortDirection },
       });
 
       if (actions) {
+        this.setGroupBy = actions.setGroupBy;
         this.setPlaylist = actions.setPlaylist;
         this.setSearchTerm = actions.setSearchTerm;
         this.setSortBy = actions.setSortBy;
+        this.setSortDirection = actions.setSortDirection;
       }
     }
 
@@ -46,6 +54,8 @@ class ScopeEngine extends BroadcastableDiffuseElement {
     const storagePrefix =
       `${this.constructor.prototype.constructor.NAME}/${this.group}`;
 
+    this.#groupBy.value =
+      localStorage.getItem(`${storagePrefix}/groupBy`) ?? undefined;
     this.#playlist.value =
       localStorage.getItem(`${storagePrefix}/playlistId`) ?? undefined;
     this.#searchTerm.value =
@@ -53,8 +63,18 @@ class ScopeEngine extends BroadcastableDiffuseElement {
     this.#sortBy.value = JSON.parse(
       localStorage.getItem(`${storagePrefix}/sortBy`) ?? "[]",
     );
+    this.#sortDirection.value =
+      localStorage.getItem(`${storagePrefix}/sortDirection`) ?? undefined;
 
     // Effects
+    this.effect(() => {
+      const key = `${storagePrefix}/groupBy`;
+      const val = this.#groupBy.value;
+
+      if (val) localStorage.setItem(key, val);
+      else localStorage.removeItem(key);
+    });
+
     this.effect(() => {
       const key = `${storagePrefix}/playlistId`;
       const val = this.#playlist.value;
@@ -78,9 +98,20 @@ class ScopeEngine extends BroadcastableDiffuseElement {
       if (val.length) localStorage.setItem(key, JSON.stringify(val));
       else localStorage.removeItem(key);
     });
+
+    this.effect(() => {
+      const key = `${storagePrefix}/sortDirection`;
+      const val = this.#sortDirection.value;
+
+      if (val) localStorage.setItem(key, val);
+      else localStorage.removeItem(key);
+    });
   }
 
   // ACTIONS
+
+  /** @param {string | undefined} val */
+  setGroupBy = async (val) => this.#groupBy.value = val;
 
   /** @param {string | undefined} val */
   setPlaylist = async (val) => this.#playlist.value = val;
@@ -90,6 +121,9 @@ class ScopeEngine extends BroadcastableDiffuseElement {
 
   /** @param {string[]} val */
   setSortBy = async (val) => this.#sortBy.value = val;
+
+  /** @param {string | undefined} val */
+  setSortDirection = async (val) => this.#sortDirection.value = val;
 }
 
 export default ScopeEngine;
