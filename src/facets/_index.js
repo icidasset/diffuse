672ff04aa5ddad54d820801e50c65af2ef8fa276
Steven Vandevelde <steven@tokono.ma>
2026-03-08 00:14:26 +0100

    wip: new your-facets-collection page

--- src/facets/_index.js
+++ src/facets/_index.js
@@ -1,8 +1,3 @@
-import { html, render } from "lit-html";
-import { keyed } from "lit-html/directives/keyed.js";
-import { marked } from "marked";
-import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
-
 import { basicSetup, EditorView } from "codemirror";
 import { css as langCss } from "@codemirror/lang-css";
 import { html as langHtml } from "@codemirror/lang-html";
@@ -13,9 +8,7 @@ import * as TID from "@atcute/tid";
 
 import * as CID from "~/common/cid.js";
 import foundation from "~/common/facets/foundation.js";
-import { effect, signal } from "~/common/signal.js";
-import { facetFromURI } from "~/common/facets/utils.js";
-import { nothing } from "~/common/element.js";
+import { signal } from "~/common/signal.js";
 import { loadURI } from "~/common/loader.js";
 
 /**
@@ -23,177 +16,10 @@ import { loadURI } from "~/common/loader.js";
  */
 
 ////////////////////////////////////////////
-// SAVE & FORK
-////////////////////////////////////////////
-
-document.body.addEventListener(
-  "click",
-  /**
-   * @param {MouseEvent} event
-   */
-  async (event) => {
-    const target = /** @type {HTMLElement} */ (event.target);
-    const rel = target.getAttribute("rel");
-    if (!rel) return;
-
-    const uri = target.closest("li")?.getAttribute("data-uri");
-    if (!uri) return;
-
-    const name = target.closest("li")?.getAttribute("data-name");
-    if (!name) return;
-
-    switch (rel) {
-      case "fork": {
-        const facet = await facetFromURI({ name, uri }, { fetchHTML: true });
-        editFacet(facet);
-        document.querySelector("#build")?.scrollIntoView();
-        break;
-      }
-      case "save": {
-        const facet = await facetFromURI({ name, uri }, { fetchHTML: false });
-        const out = foundation.orchestrator.output();
-
-        out.facets.save([
-          ...out.facets.collection(),
-          facet,
-        ]);
-        break;
-      }
-    }
-  },
-);
-
-////////////////////////////////////////////
-// YOUR COLLECTION
-////////////////////////////////////////////
-
-/** @type {HTMLElement | null} */
-const listEl = document.querySelector("#list");
-if (!listEl) throw new Error("List element not found");
-
-const output = foundation.orchestrator.output();
-
-listEl.innerHTML = "";
-
-effect(() => {
-  const col = output.facets.collection().sort((a, b) => {
-    return a.name.toLocaleLowerCase().localeCompare(b.name.toLocaleLowerCase());
-  });
-
-  const state = output.facets.state();
-
-  const h = col.length && state === "loaded"
-    ? html`
-      <ul>
-        ${col.map((c) =>
-          keyed(
-            c.id,
-            html`
-              <li>
-                <div style="position: relative;">
-                  <a href="facets/l/?id=${c.id}">
-                    ${c.name}
-                  </a>
-                  <button
-                    class="button--fixed button--transparent"
-                    popovertarget="facet-menu-col-${c.id}"
-                    style="anchor-name: --facet-anchor-col-${c
-                      .id}; position: absolute; right: 0; top: 50%; transform: translateY(-50%);"
-                  >
-                    <i class="ph-fill ph-dots-three-circle"></i>
-                  </button>
-                </div>
-                <div class="list-description">
-                  <div>
-                    ${c.description?.trim().length
-                      ? unsafeHTML(
-                        marked.parse(c.description, { async: false }),
-                      )
-                      : nothing}
-                  </div>
-                  <div>
-                    ${c.uri && !c.html
-                      ? html`
-                        <span class="with-icon">
-                          <i class="ph-fill ph-binoculars"></i>
-                          <span>Tracking the original <a href="${c
-                            .uri}">URI</a></span>
-                        </span>
-                      `
-                      : html`
-                        <span class="with-icon">
-                          <i class="ph-fill ph-code"></i>
-                          <span>Custom code</span>
-                        </span>
-                      `}
-                  </div>
-                </div>
-
-                <!-- Dropdown Menu -->
-                <div
-                  id="facet-menu-col-${c.id}"
-                  class="dropdown"
-                  style="position-anchor: --facet-anchor-col-${c.id}"
-                  popover
-                >
-                  <a href="facets/l/?id=${c.id}">
-                    <span class="with-icon">
-                      <i class="ph-fill ph-globe"></i> Open
-                    </span>
-                  </a>
-                  <a @click="${() => editFacet(c)}">
-                    <span class="with-icon">
-                      <i class="ph-fill ph-cursor-text"></i> Edit
-                    </span>
-                  </a>
-                  <a @click="${deleteFacet({ id: c.id })}">
-                    <span class="with-icon">
-                      <i class="ph-fill ph-eraser"></i> Delete
-                    </span>
-                  </a>
-                </div>
-              </li>
-            `,
-          )
-        )}
-      </ul>
-    `
-    : state === "loaded"
-    ? emptyFacetsList
-    : html`
-      <div class="with-icon" style="font-size: var(--fs-sm);">
-        <i class="ph-bold ph-spinner-gap"></i>
-        Loading items
-      </div>
-    `;
-
-  render(h, listEl);
-});
-
-const emptyFacetsList = html`
-  <p style="margin-bottom: 0;">
-    <i class="ph-fill ph-info"></i> You have not saved any facets yet.
-  </p>
-`;
-
-/**
- * @param {{ id: string }} _
- */
-function deleteFacet({ id }) {
-  return () => {
-    const c = confirm("Are you sure you want to delete this facet?");
-    if (!c) return;
-
-    output.facets.save(
-      output.facets.collection().filter((c) => !(c.id === id)),
-    );
-  };
-}
-
-////////////////////////////////////////////
 // BUILD
 ////////////////////////////////////////////
 
+const output = foundation.orchestrator.output();
 const $editingFacet = signal(/** @type {Facet | null} */ (null));
 
 // Code editor
---
+++ src/facets/you.js
@@ -0,0 +1,138 @@
+import { html, render } from "lit-html";
+import { keyed } from "lit-html/directives/keyed.js";
+import { marked } from "marked";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
+
+import * as Output from "~/common/output.js";
+import foundation from "~/common/facets/foundation.js";
+import { effect } from "~/common/signal.js";
+import { nothing } from "~/common/element.js";
+
+////////////////////////////////////////////
+// YOUR COLLECTION
+////////////////////////////////////////////
+
+/** @type {HTMLElement | null} */
+const listEl = document.querySelector("#list");
+if (!listEl) throw new Error("List element not found");
+
+const output = foundation.orchestrator.output();
+
+listEl.innerHTML = "";
+
+effect(() => {
+  const col = output.facets.collection().sort((a, b) => {
+    return a.name.toLocaleLowerCase().localeCompare(b.name.toLocaleLowerCase());
+  });
+
+  const state = output.facets.state();
+
+  const h = col.length && state === "loaded"
+    ? html`
+      <ul style="margin: 0; max-width: none;">
+        ${col.map((c) =>
+          keyed(
+            c.id,
+            html`
+              <li style="margin-bottom: var(--space-sm)">
+                <div style="position: relative;">
+                  <a href="facets/l/?id=${c.id}">
+                    ${c.name}
+                  </a>
+                  <button
+                    class="button--fixed button--transparent"
+                    popovertarget="facet-menu-col-${c.id}"
+                    style="anchor-name: --facet-anchor-col-${c
+                      .id}; position: absolute; right: 0; top: 50%; transform: translateY(-50%);"
+                  >
+                    <i class="ph-fill ph-dots-three-circle"></i>
+                  </button>
+                </div>
+                <div class="list-description">
+                  <div>
+                    ${c.description?.trim().length
+                      ? unsafeHTML(
+                        marked.parse(c.description, { async: false }),
+                      )
+                      : nothing}
+                  </div>
+                  <div>
+                    ${c.uri && !c.html
+                      ? html`
+                        <span class="with-icon">
+                          <i class="ph-fill ph-binoculars"></i>
+                          <span>Tracking the original <a href="${c
+                            .uri}">URI</a></span>
+                        </span>
+                      `
+                      : html`
+                        <span class="with-icon">
+                          <i class="ph-fill ph-code"></i>
+                          <span>Custom code</span>
+                        </span>
+                      `}
+                  </div>
+                </div>
+
+                <!-- Dropdown Menu -->
+                <div
+                  id="facet-menu-col-${c.id}"
+                  class="dropdown"
+                  style="position-anchor: --facet-anchor-col-${c.id}"
+                  popover
+                >
+                  <a href="facets/l/?id=${c.id}">
+                    <span class="with-icon">
+                      <i class="ph-fill ph-globe"></i> Open
+                    </span>
+                  </a>
+                  <a @click="${() => editFacet(c)}">
+                    <span class="with-icon">
+                      <i class="ph-fill ph-cursor-text"></i> Edit
+                    </span>
+                  </a>
+                  <a @click="${deleteFacet({ id: c.id })}">
+                    <span class="with-icon">
+                      <i class="ph-fill ph-eraser"></i> Delete
+                    </span>
+                  </a>
+                </div>
+              </li>
+            `,
+          )
+        )}
+      </ul>
+    `
+    : state === "loaded"
+    ? emptyFacetsList
+    : html`
+      <div class="with-icon">
+        <i class="ph-bold ph-spinner-gap"></i>
+        Loading items
+      </div>
+    `;
+
+  render(h, listEl);
+});
+
+const emptyFacetsList = html`
+  <p style="margin-bottom: 0;">
+    <i class="ph-fill ph-info"></i> You have not saved any facets yet.
+  </p>
+`;
+
+/**
+ * @param {{ id: string }} _
+ */
+function deleteFacet({ id }) {
+  return async () => {
+    const c = confirm("Are you sure you want to delete this facet?");
+    if (!c) return;
+
+    await Output.waitUntilLoaded(output.facets);
+
+    output.facets.save(
+      output.facets.collection().filter((c) => !(c.id === id)),
+    );
+  };
+}
