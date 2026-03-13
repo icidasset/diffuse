e00f5745d692fd9f47990bda5d09880c0d723bdb
Steven Vandevelde <steven@tokono.ma>
2026-03-14 00:00:19 +0100

    chore: _data facets as json + determine defaults

--- src/common/facets/constants.js
+++ src/common/facets/constants.js
@@ -1,16 +1,42 @@
+import facets from "../../_data/facets.json" with {
+  type: "json",
+};
+
 /**
  * @import {Facet} from "~/definitions/types.d.ts"
  */
 
+export const TYPE = /** @type {const} */ ("sh.diffuse.output.facet");
+
 /** @type {Facet[]} */
-export const STARTING_SET = [
-  {
-    $type: "sh.diffuse.output.facet",
-    id: "default/process-tracks",
-    kind: "prelude",
-    name: "Process Tracks",
-    uri: "diffuse://facets/data/process-tracks/index.html",
-    description:
-      "Process all your audio inputs automatically when opening any interface facet.",
-  },
-];
+export const STARTING_SET = facets.flatMap((facet) => {
+  const properties = {
+    $type: TYPE,
+    description: facet.desc,
+    kind: facet.kind === "prelude"
+      ? /** @type {const} */ ("prelude")
+      : /** @type {const} */ ("interactive"),
+    name: facet.title,
+    uri: "diffuse://" + facet.url,
+  };
+
+  switch (facet.url) {
+    case "facets/data/input-bundle/index.html":
+      return [{
+        ...properties,
+        id: "defaults/input-bundle",
+      }];
+    case "facets/data/output-bundle/index.html":
+      return [{
+        ...properties,
+        id: "defaults/output-bundle",
+      }];
+    case "facets/data/process-tracks/index.html":
+      return [{
+        ...properties,
+        id: "defaults/process-tracks",
+      }];
+    default:
+      return [];
+  }
+});
