944aef04ea61ba3a3358d75e60be79e660bf9ffb
Steven Vandevelde <steven@tokono.ma>
2026-03-17 16:48:58 +0100

    chore: throttle processing facet by 10 minutes

--- src/facets/data/process-tracks/index.inline.js
+++ src/facets/data/process-tracks/index.inline.js
@@ -1,5 +1,23 @@
 import foundation from "~/common/foundation.js";
 
-foundation.orchestrator.processTracks({
-  disableWhenReady: false,
-});
+const KEY = "facets/data/process-tracks/timestamp";
+const lastTimestamp = localStorage.getItem(KEY);
+const now = Date.now();
+
+if (lastTimestamp) {
+  const diff = now - JSON.parse(lastTimestamp);
+
+  if (diff >= 10 * 60 * 1000) {
+    foundation.orchestrator.processTracks({
+      disableWhenReady: false,
+    });
+
+    localStorage.setItem(KEY, JSON.stringify(now));
+  }
+} else {
+  foundation.orchestrator.processTracks({
+    disableWhenReady: false,
+  });
+
+  localStorage.setItem(KEY, JSON.stringify(now));
+}
