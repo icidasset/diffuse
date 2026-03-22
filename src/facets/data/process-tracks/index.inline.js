e589e1ed937521312292a071554fd0d001390b6a
Steven Vandevelde <steven@tokono.ma>
2026-03-22 22:14:36 +0100

    chore: improve the process-tracks prelude

--- src/facets/data/process-tracks/index.inline.js
+++ src/facets/data/process-tracks/index.inline.js
@@ -1,23 +1,18 @@
 import foundation from "~/common/foundation.js";
 
 const KEY = "facets/data/process-tracks/timestamp";
+const MAX_TIME_DIFF = 10 * 60 * 1000;
+
 const lastTimestamp = localStorage.getItem(KEY);
 const now = Date.now();
+const diff = lastTimestamp ? now - JSON.parse(lastTimestamp) : MAX_TIME_DIFF;
 
-if (lastTimestamp) {
-  const diff = now - JSON.parse(lastTimestamp);
-
-  if (diff >= 10 * 60 * 1000) {
-    foundation.orchestrator.processTracks({
-      disableWhenReady: false,
-    });
-
-    localStorage.setItem(KEY, JSON.stringify(now));
-  }
-} else {
-  foundation.orchestrator.processTracks({
-    disableWhenReady: false,
+if (diff >= MAX_TIME_DIFF) {
+  const orchestrator = await foundation.orchestrator.processTracks({
+    disableWhenReady: true,
   });
 
+  // Wait until we're actually done processing, only then set the timestamp
+  await orchestrator.process();
   localStorage.setItem(KEY, JSON.stringify(now));
 }
