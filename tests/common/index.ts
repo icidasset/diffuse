aea0e22636108361aade56129250a81c46e11291
Steven Vandevelde <steven@tokono.ma>
2026-05-21 18:32:55 +0200

    test: coverage commands

--- tests/common/index.ts
+++ tests/common/index.ts
@@ -4,7 +4,7 @@ export async function testWeb<T>(evalFn: EvaluateFunction<T, []>): Promise<T> {
   const url = "http://localhost:3000/testing/index.html";
 
   await using browser = await launch();
-  await using page = await browser.newPage(url);
+  await using page = await browser.newPage(url, { coverage: true });
 
   return await page.evaluate(evalFn);
 }
